8cef59f009925f15e648e6b7a58d3eae0f999740
wen <1175445093@qq.com>
2022-05-16 18:52:55 +0800

    提交

--- lua/conf/nvim-lsp-installer.lua
+++ lua/conf/nvim-lsp-installer.lua
@@ -12,6 +12,7 @@ capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 local servers = {
     -- 语言服务器名称：配置选项
+    clangd      = require("lsp.clangd"),
     -- sumneko_lua = require("lsp.sumneko_lua"),
     -- pyright = require("lsp.pyright"),
     -- tsserver = require("lsp.tsserver"),
@@ -28,20 +29,28 @@ local servers = {
 local function attach(_, bufnr)
     -- 跳转到定义（代替内置 LSP 的窗口，telescope 插件让跳转定义更方便）
     vim.keybinds.bmap(bufnr, "n", "gd", "<cmd>Telescope lsp_definitions theme=dropdown<CR>", vim.keybinds.opts)
+
     -- 列出光标下所有引用（代替内置 LSP 的窗口，telescope 插件让查看引用更方便）
     vim.keybinds.bmap(bufnr, "n", "gr", "<cmd>Telescope lsp_references theme=dropdown<CR>", vim.keybinds.opts)
+
     -- 工作区诊断（代替内置 LSP 的窗口，telescope 插件让工作区诊断更方便）
     vim.keybinds.bmap(bufnr, "n", "go", "<cmd>Telescope diagnostics theme=dropdown<CR>", vim.keybinds.opts)
+
     -- 显示代码可用操作（代替内置 LSP 的窗口，telescope 插件让代码行为更方便）
     vim.keybinds.bmap(bufnr, "n", "<leader>ca", "<cmd>Telescope lsp_code_actions theme=dropdown<CR>", vim.keybinds.opts)
+
     -- 变量重命名（代替内置 LSP 的窗口，Lspsaga 让变量重命名更美观）
     vim.keybinds.bmap(bufnr, "n", "<leader>cn", "<cmd>Lspsaga rename<CR>", vim.keybinds.opts)
+
     -- 查看帮助信息（代替内置 LSP 的窗口，Lspsaga 让查看帮助信息更美观）
     vim.keybinds.bmap(bufnr, "n", "gh", "<cmd>Lspsaga hover_doc<CR>", vim.keybinds.opts)
+
     -- 跳转到上一个问题（代替内置 LSP 的窗口，Lspsaga 让跳转问题更美观）
     vim.keybinds.bmap(bufnr, "n", "g[", "<cmd>Lspsaga diagnostic_jump_prev<CR>", vim.keybinds.opts)
+
     -- 跳转到下一个问题（代替内置 LSP 的窗口，Lspsaga 让跳转问题更美观）
     vim.keybinds.bmap(bufnr, "n", "g]", "<cmd>Lspsaga diagnostic_jump_next<CR>", vim.keybinds.opts)
+
     -- 悬浮窗口上翻页，由 Lspsaga 提供
     vim.keybinds.bmap(
         bufnr,
--- lua/conf/telescope.lua
+++ lua/conf/telescope.lua
@@ -1,6 +1,6 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
--- WARN: telescope 手动安装依赖 fd 和 repgrep
+-- WARN: telescope 手动安装依赖 fd 和 ripgrep
 -- https://github.com/sharkdp/fd
 -- https://github.com/BurntSushi/ripgrep
 
