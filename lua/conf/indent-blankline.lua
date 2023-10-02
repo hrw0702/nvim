3a52bf7762838766179ec2de083876d5da17404d
wen <1175445093@qq.com>
2023-10-02 13:31:14 +0800

    update V3

--- lua/conf/indent-blankline.lua
+++ lua/conf/indent-blankline.lua
@@ -3,13 +3,68 @@
 -- vim.opt.list = true
 -- vim.opt.listchars:append("eol:↴")
 
-require("indent_blankline").setup(
-    {
-        -- 显示当前所在区域
-        show_current_context = true,
-        -- 显示当前所在区域的开始位置
-        show_current_context_start = false,
-        -- 显示行尾符
-        show_end_of_line = true
-    }
-)
+require("ibl").setup({
+	enabled = true,
+	debounce = 200,
+	viewport_buffer = {
+		min = 30,
+		max = 500,
+	},
+	indent = {
+		char = "│",
+		-- tab_char = ">",
+		-- highlight = "IblIndent",
+		smart_indent_cap = true,
+		priority = 1024,
+	},
+	whitespace = {
+		-- highlight = "IblWhitespace",
+		remove_blankline_trail = true,
+	},
+	scope = {
+		enabled = true,
+		char = nil,
+		show_start = false,
+		show_end = false,
+		injected_languages = true,
+		highlight = "IblScope",
+		priority = 1024,
+		include = {
+			node_type = {},
+		},
+		exclude = {
+			language = {},
+			node_type = {
+				["*"] = {
+					"source_file",
+					"program",
+				},
+				lua = {
+					"chunk",
+				},
+				python = {
+					"module",
+				},
+			},
+		},
+	},
+	exclude = {
+		filetypes = {
+			"lspinfo",
+			"packer",
+			"checkhealth",
+			"help",
+			"man",
+			"gitcommit",
+			"TelescopePrompt",
+			"TelescopeResults",
+			"",
+		},
+		buftypes = {
+			"terminal",
+			"nofile",
+			"quickfix",
+			"prompt",
+		},
+	},
+})
