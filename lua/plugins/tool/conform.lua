2bf0c33bd1d014a8bf8d462f352316f046e61894
hrw0702 <1175445093@qq.com>
2024-09-22 21:32:50 +0800

    refactor: 重新格式化文件

--- lua/plugins/tool/conform.lua
+++ lua/plugins/tool/conform.lua
@@ -1,51 +1,51 @@
 ---@type LazyPluginSpec
 return {
-  "stevearc/conform.nvim",
-  cmd = "ConformInfo",
-  keys = {
-    {
-      "<leader>F",
-      function()
-        require("conform").format { lsp_format = "fallback" }
-      end,
-      desc = "Format Document",
-      mode = { "n", "v" },
+    "stevearc/conform.nvim",
+    cmd = "ConformInfo",
+    keys = {
+        {
+            "<leader>F",
+            function()
+                require("conform").format { lsp_format = "fallback" }
+            end,
+            desc = "Format Document",
+            mode = { "n", "v" },
+        },
     },
-  },
-  config = function()
-    require("conform").setup {
-      formatters_by_ft = {
-        cpp = { "clang_format" },
-        python = { "ruff_fix", "ruff_format" },
-        lua = { "stylua" },
-        rust = { "rustfmt" },
-        html = { "prettier" },
-        astro = { "prettier" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        markdown = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        yaml = { "prettier" },
-        typst = { "typstyle" },
-        nix = { "nixfmt" },
-        haskell = { "fourmolu" },
-      },
-      formatters = {
-        clang_format = {
-          command = "clang-format",
-          prepend_args = {
-            "-style=file:/home/hrw0702/C_Program/.clang-format",
-          },
-        },
-        prettier = {
-          prepend_args = {
-            "--tab-width 2",
-          },
-        },
-      },
-    }
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    vim.o.formatprg = "v:lua.require'conform'.formatprg()"
-  end,
+    config = function()
+        require("conform").setup {
+            formatters_by_ft = {
+                cpp = { "clang_format" },
+                python = { "ruff_fix", "ruff_format" },
+                lua = { "stylua" },
+                rust = { "rustfmt" },
+                html = { "prettier" },
+                astro = { "prettier" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                markdown = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                yaml = { "prettier" },
+                typst = { "typstyle" },
+                nix = { "nixfmt" },
+                haskell = { "fourmolu" },
+            },
+            formatters = {
+                clang_format = {
+                    command = "clang-format",
+                    prepend_args = {
+                        "-style=file:/home/hrw0702/C_Program/.clang-format",
+                    },
+                },
+                prettier = {
+                    prepend_args = {
+                        "--tab-width 4",
+                    },
+                },
+            },
+        }
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+        vim.o.formatprg = "v:lua.require'conform'.formatprg()"
+    end,
 }
