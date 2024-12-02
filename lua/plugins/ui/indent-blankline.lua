37ffee6a66c0bd91992e1b2758dc57effe270681
hrw0702 <1175445093@qq.com>
2024-12-02 08:27:24 +0800

    refactor:修改blink.cmp配置

--- lua/plugins/ui/indent-blankline.lua
+++ lua/plugins/ui/indent-blankline.lua
@@ -1,99 +1,63 @@
--- ---@type LazyPluginSpec
--- return {
---   "lukas-reineke/indent-blankline.nvim",
---   event = "VeryLazy",
---   opts = {
---     indent = {
---       char = "│",
---       tab_char = "│",
---       -- highlight = "IblIndent",
---       smart_indent_cap = true,
---       priority = 1,
---     },
---     whitespace = {
---       -- highlight = "IblWhitespace",
---       remove_blankline_trail = true,
---     },
---     scope = {
---       enabled = true,
---       char = nil,
---       show_start = false,
---       show_end = false,
---       injected_languages = false,
---       highlight = "IblScope",
---       priority = 500,
---       include = {
---         node_type = {
---           ["*"] = {
---             "*",
---           },
---         },
---       },
---     },
---   },
---   config = function(_, opts)
---     local ibl = require "ibl"
---     local hooks = require "ibl.hooks"
---
---     ibl.setup(opts)
---     hooks.register(
---       hooks.type.SCOPE_HIGHLIGHT,
---       hooks.builtin.scope_highlight_from_extmark
---     )
---
---     -- -- Hide first level indent, using `foldsep` to show it
---     -- hooks.register(hooks.type.VIRTUAL_TEXT, function(_, bufnr, row, virt_text)
---     --   local win = vim.fn.bufwinid(bufnr)
---     --   local lnum = row + 1
---     --   local foldinfo = utils.fold_info(win, lnum)
---     --
---     --   if
---     --     virt_text[1]
---     --     and virt_text[1][1] == opts.indent.char
---     --     and foldinfo
---     --     and foldinfo.start == lnum
---     --   then
---     --     virt_text[1] = { " ", { "@ibl.whitespace.char.1" } }
---     --   end
---     --
---     --   return virt_text
---     -- end)
---   end,
--- }
-local utils = require "utils"
-
 ---@type LazyPluginSpec
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = "VeryLazy",
-  opts = {
-    indent = {
-      char = "▏", -- Thiner, not suitable when enable scope
-      tab_char = "▏",
+    "lukas-reineke/indent-blankline.nvim",
+    event = "VeryLazy",
+    opts = {
+        indent = {
+            char = "│",
+            tab_char = "│",
+            -- highlight = "IblIndent",
+            smart_indent_cap = true,
+            priority = 1,
+        },
+        whitespace = {
+            -- highlight = "IblWhitespace",
+            remove_blankline_trail = true,
+        },
+        scope = {
+            enabled = true,
+            char = nil,
+            show_start = false,
+            show_end = false,
+            injected_languages = false,
+            highlight = "IblScope",
+            priority = 500,
+            include = {
+                node_type = {
+                    ["*"] = {
+                        "*",
+                    },
+                },
+            },
+        },
     },
-  },
-  config = function(_, opts)
-    local ibl = require "ibl"
-    local hooks = require "ibl.hooks"
 
-    ibl.setup(opts)
+    config = function(_, opts)
+        local utils = require "utils"
+        local ibl = require "ibl"
+        local hooks = require "ibl.hooks"
 
-    -- Hide first level indent, using `foldsep` to show it
-    hooks.register(hooks.type.VIRTUAL_TEXT, function(_, bufnr, row, virt_text)
-      local win = vim.fn.bufwinid(bufnr)
-      local lnum = row + 1
-      local foldinfo = utils.fold_info(win, lnum)
+        ibl.setup(opts)
 
-      if
-        virt_text[1]
-        and virt_text[1][1] == opts.indent.char
-        and foldinfo
-        and foldinfo.start == lnum
-      then
-        virt_text[1] = { " ", { "@ibl.whitespace.char.1" } }
-      end
+        -- Hide first level indent, using `foldsep` to show it
+        hooks.register(
+            hooks.type.VIRTUAL_TEXT,
+            function(_, bufnr, row, virt_text)
+                local win = vim.fn.bufwinid(bufnr)
+                local lnum = row + 1
+                local foldinfo = utils.fold_info(win, lnum)
 
-      return virt_text
-    end)
-  end,
+                if
+                    virt_text[1]
+                    and virt_text[1][1] == opts.indent.char
+                    and foldinfo
+                    and foldinfo.start == lnum
+                then
+                    virt_text[1] = { " ", { "@ibl.whitespace.char.1" } }
+                end
+
+                return virt_text
+            end
+        )
+    end,
 }
